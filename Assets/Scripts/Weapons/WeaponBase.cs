86cf9fa7ea8455f9dfef3ff424c6ead01a06b2ad
sadjin25 <qkrdnwls0101@naver.com>
2023-04-01 19:33:02 +0900

    fix: make weaponbase class 'abstract'

--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponBase : MonoBehaviour
+public abstract class WeaponBase : MonoBehaviour
 {
     protected WeaponData weaponData;
 
