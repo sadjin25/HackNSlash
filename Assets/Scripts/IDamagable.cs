2601ee7740ba15671cd352c41cad8df1e62d84b1
sadjin25 <qkrdnwls0101@naver.com>
2023-04-01 19:51:12 +0900

    fix: remove IDamagable's monobehavior keyword

--- Assets/Scripts/IDamagable.cs
+++ Assets/Scripts/IDamagable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IDamagable : MonoBehavior
+public interface IDamagable
 {
     public bool GetDamage(float damage);
 }
